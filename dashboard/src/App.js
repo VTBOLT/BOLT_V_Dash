21874a997cd6f0a1c83ba4f1bfe00efe2b943cd6
jcsmeth <jcsmeth@vt.edu>
2020-02-17 13:28:09 -0500

    CAN listener skeleton

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import RPMBar from './RPMBar.js';
+import CANListener from './CANListener.js';
 
-function App() {
-  return (
-    <div className="App">
-      <RPMBar />
-    </div>
-  );
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <RPMBar />
+
+        <CANListener />
+      </div>
+    );
+  }
 }
 
 export default App;
--- dashboard/src/RPMBar.js
+++ dashboard/src/RPMBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const rpmPath = 'M 15 120 L 60 90 C 135 45 180 30 555 30 L 780 30 ';
+const rpmPath = 'M 15 120 L 60 90 C 135 45 180 30 285 30 L 780 30';
 const svgBox = '0 0 800 150';
 const rpmWidth = '50';
 const rpmBarLength = 783;
@@ -10,17 +10,18 @@ class RPMBar extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = { rpm: 5000.0 };
+        this.state = { rpm: 0.0 };
     }
 
     /**
      * Takes in an rpm value and returns a string representation of an SVG path dasharray
      * with a length corresponding to the percentage that rpm value is compared to the max rpm
+     * TODO enhance this for nonlinear rpm growth
      * 
      * @param {int} rpm the new rpm value
      */
     getDashArray(rpm) {
-        return '' + (this.state.rpm / maxRPM) * rpmBarLength + ' ' + rpmBarLength;
+        return (this.state.rpm / maxRPM) * rpmBarLength + ' ' + rpmBarLength;
     }
 
     render() {
